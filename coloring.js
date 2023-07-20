7810926530e0a5597535ce8e48c8469b1ed02362
alesikivan <obcofficiall@gmail.com>
2023-07-20 21:15:55 +0300

    app init

---
+++ coloring.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+const path = require('path')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+
+app.use(express.static(path.join(__dirname, 'dist')))
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+})
+
+const PORT = process.env.PORT || 3002
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
